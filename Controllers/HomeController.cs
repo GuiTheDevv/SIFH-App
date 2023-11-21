c208dfd4a17b383bb5aa36e3a23718eff3e98ab9
GuiTheDevv <104726381+GuiTheDevv@users.noreply.github.com>
2023-11-21 16:43:55 -0400

    SIFH App

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+// using SIFH_Application.Models;
+
+namespace SIFH_Application.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController()
+    {
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
